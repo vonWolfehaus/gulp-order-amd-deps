7eecd7406091c01c9ddc9ed57e4a117b55d6c53d
nevyk <petr.nevyhosteny@gmail.com>
2014-06-13 09:51:56 +0000

    Replace tabs with four spaces

--- test.js
+++ test.js
@@ -4,7 +4,7 @@ var gutil = require('gulp-util');
 var depsOrder = require('./index');
 
 it('should sort files by dependencies defined using @requires', function (cb) {
-	var order = depsOrder();
+    var order = depsOrder();
     var original = order.findOrder();
     var ordered = order.sortFiles();
     
@@ -32,62 +32,62 @@ it('should sort files by dependencies defined using @requires', function (cb) {
     
     var files = [];
 
-	ordered.on('data', function (file) {
+    ordered.on('data', function (file) {
         assert(depsGraph[file.relative]);
         
         depsGraph[file.relative].forEach(function (dep) {
             assert(files.indexOf(dep) !== -1);
         })
-		
+        
         files.push(file.relative);
-	});
+    });
 
-	ordered.on('end', function () {
+    ordered.on('end', function () {
         assert.equal(files.length, 6);
         cb();
     });
 
-	original.write(new gutil.File({
-		base: __dirname,
-		path: __dirname + '/a.js',
-		contents: new Buffer('/* @requires sub/b.js, c.js\n */')
-	}));
+    original.write(new gutil.File({
+        base: __dirname,
+        path: __dirname + '/a.js',
+        contents: new Buffer('/* @requires sub/b.js, c.js\n */')
+    }));
     
     original.write(new gutil.File({
-		base: __dirname,
-		path: __dirname + '/d.js',
-		contents: new Buffer('/* @requires e.js\n */')
-	}));
+        base: __dirname,
+        path: __dirname + '/d.js',
+        contents: new Buffer('/* @requires e.js\n */')
+    }));
     
     original.write(new gutil.File({
-		base: __dirname,
-		path: __dirname + '/e.js',
-		contents: new Buffer('')
-	}));
+        base: __dirname,
+        path: __dirname + '/e.js',
+        contents: new Buffer('')
+    }));
     
     original.write(new gutil.File({
-		base: __dirname,
-		path: __dirname + '/f.js',
-		contents: new Buffer('')
-	}));
+        base: __dirname,
+        path: __dirname + '/f.js',
+        contents: new Buffer('')
+    }));
     
     original.write(new gutil.File({
-		base: __dirname,
-		path: __dirname + '/sub/b.js',
-		contents: new Buffer('/* @requires ../e.js\n */')
-	}));
+        base: __dirname,
+        path: __dirname + '/sub/b.js',
+        contents: new Buffer('/* @requires ../e.js\n */')
+    }));
     
     original.write(new gutil.File({
-		base: __dirname,
-		path: __dirname + '/sub/c.js',
-		contents: new Buffer('/* @requires b.js, ../d.js\n */')
-	}));
+        base: __dirname,
+        path: __dirname + '/sub/c.js',
+        contents: new Buffer('/* @requires b.js, ../d.js\n */')
+    }));
 
-	original.end();
+    original.end();
 });
 
 it('should emit an error if there is a cyclic dependency', function (cb) {
-	var order = depsOrder();
+    var order = depsOrder();
     var original = order.findOrder();
     var ordered = order.sortFiles();
     
@@ -103,7 +103,7 @@ it('should emit an error if there is a cyclic dependency', function (cb) {
     
     var orderedFiles = ['e.js', 'd.js', 'sub/b.js', 'sub/c.js', 'a.js'];
 
-	original.on('error', function (err) {
+    original.on('error', function (err) {
         assert.notEqual(err, undefined);
         cb();
     });
@@ -113,29 +113,29 @@ it('should emit an error if there is a cyclic dependency', function (cb) {
         cb();
     });
 
-	original.write(new gutil.File({
-		base: __dirname,
-		path: __dirname + '/a.js',
-		contents: new Buffer('/* @requires d.js\n */')
-	}));
+    original.write(new gutil.File({
+        base: __dirname,
+        path: __dirname + '/a.js',
+        contents: new Buffer('/* @requires d.js\n */')
+    }));
     
     original.write(new gutil.File({
-		base: __dirname,
-		path: __dirname + '/b.js',
-		contents: new Buffer('/* @requires a.js\n */')
-	}));
+        base: __dirname,
+        path: __dirname + '/b.js',
+        contents: new Buffer('/* @requires a.js\n */')
+    }));
     
     original.write(new gutil.File({
-		base: __dirname,
-		path: __dirname + '/c.js',
-		contents: new Buffer('/* @requires b.js\n */')
-	}));
+        base: __dirname,
+        path: __dirname + '/c.js',
+        contents: new Buffer('/* @requires b.js\n */')
+    }));
     
     original.write(new gutil.File({
-		base: __dirname,
-		path: __dirname + '/d.js',
-		contents: new Buffer('/* @requires c.js\n */')
-	}));
+        base: __dirname,
+        path: __dirname + '/d.js',
+        contents: new Buffer('/* @requires c.js\n */')
+    }));
 
-	original.end();
+    original.end();
 });

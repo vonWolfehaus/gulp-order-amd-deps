d778710d95e0d82de68bd767f063b7430815c330
Corey Birnbaum <vonwolfehaus@gmail.com>
2016-01-03 18:16:36 -0800

    added optional global param

--- shim/define.js
+++ shim/define.js
@@ -1,23 +1,35 @@
 (function() {
 	var _map = {};
 
+	// only to be used if a global was not in the parameters of the define call
 	function get(key) {
 		if (_map.hasOwnProperty(key)) return _map[key];
 		throw '[require-shim] Cannot find module "'+key+'"';
 	}
 
-	function set(key, module) {
-		if (_map.hasOwnProperty(key)) {
+	// global is an optional object and will attach the constructed module to it if present
+	function set(key, module, global) {
+		var m = null;
+		var isGlobal = global && typeof global !== 'undefined';
+
+		// check to make sure the module has been assigned only once
+		if (isGlobal) {
+			if (global.hasOwnProperty(key)) {
+				throw '[define-shim] Module ' + key + ' already exists';
+			}
+		}
+		else if (_map.hasOwnProperty(key)) {
 			throw '[define-shim] Module ' + key + ' already exists';
 			return;
 		}
-		
-		if (typeof module === 'function') {
-			_map[key] = module(get);
-		}
-		else {
-			_map[key] = module;
-		}
+
+		// construct the module if needed
+		if (typeof module === 'function') m = module(get);
+		else m = module;
+
+		// assign the module to whichever object is preferred
+		if (isGlobal) global[key] = m;
+		else _map[key] = m;
 	}
 
 	window.define = set;
